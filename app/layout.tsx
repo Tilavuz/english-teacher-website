52446219eff7525cf8f72638ab46effdef984795
Tilavuz <tilav777@gamil.com>
2025-04-09 09:13:38 +0500

    some changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
+// app/layout.tsx
+
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "IELTS with Maxmudov. M.",
   description: "Maxmudov Mirasror",
+  verification: {
+    google: "L-7zlnYOZGRbdV_75f-GYgldZY7-Y_K17fi1SckoybE",
+  },
 };
 
 export default function RootLayout({
